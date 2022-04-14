99f8b7c09cec6837a5e08f1ed1f95cd7ca35bd8b
johannes-ws <johannessukiatmodjo20@gmail.com>
2022-04-15 02:46:18 +0700

    menambahkan kelas Board dan Deck

---
+++ src/main/java/com/aetherwars/board/Board.java
@@ -0,0 +1,31 @@
+package com.aetherwars.board;
+
+import java.util.Map;
+
+public class Board {
+//    private Map<String, CharacterCard> field;
+
+    public Board() {
+
+    }
+
+//    public void putCharacterCard(CharacterCard c, String slot) {
+//
+//    }
+
+//    public void putSpellCard(SpellCard s, String slot) {
+//
+//    }
+
+    public void attack(String attackerSlot, String targetSlot) {
+
+    }
+
+    public void destroy(String slot) {
+
+    }
+
+//    public boolean characterExists(String slot) {
+//
+//    }
+}
---
+++ src/main/java/com/aetherwars/deck/Deck.java
@@ -0,0 +1,25 @@
+package com.aetherwars.deck;
+
+import java.util.List;
+
+public class Deck {
+//    private List<Card> card_list;
+    private int count;
+    private static int max;
+
+    public Deck() {
+
+    }
+
+//    public List<Card> draw() {
+//
+//    }
+
+    public void shuffle() {
+
+    }
+
+    public void returnCard() {
+
+    }
+}
