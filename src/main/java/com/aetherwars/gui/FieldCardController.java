a2431d7df14e9ea7b438baf0d6f95351d752dddb
ignferry <ignasius.ferry01@gmail.com>
2022-04-19 07:33:56 +0700

    Add assets

---
+++ src/main/java/com/aetherwars/gui/FieldCardController.java
@@ -0,0 +1,14 @@
+package com.aetherwars.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class FieldCardController {
+    @FXML private ImageView typeImageView;
+    @FXML private Label atkLabel;
+    @FXML private Label HPLabel;
+    @FXML private Label levelLabel;
+    @FXML private Label expLabel;
+    @FXML private ImageView cardImageView;
+}
