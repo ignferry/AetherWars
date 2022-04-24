60a02baec52a71933687fb146ed1bad4be226490
ignferry <ignasius.ferry01@gmail.com>
2022-04-24 18:22:12 +0700

    Fix gradle not working bug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.aetherwars.aetherwars {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.aetherwars to javafx.fxml;
     exports com.aetherwars;
+
+    opens com.aetherwars.gui to javafx.fxml;
 }
