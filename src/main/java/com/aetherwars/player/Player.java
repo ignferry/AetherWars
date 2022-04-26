632d23fcd5fb182e3b8245b3d85755d8c85bf2eb
ignferry <ignasius.ferry01@gmail.com>
2022-04-27 03:51:37 +0700

    Add player class

---
+++ src/main/java/com/aetherwars/player/Player.java
@@ -0,0 +1,62 @@
+package com.aetherwars.player;
+
+import com.aetherwars.card.Card;
+import com.aetherwars.deck.Deck;
+import com.aetherwars.gamestate.GameState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private final int id;
+    private final String name;
+    private int hp;
+    private int mana;
+    private List<Card> hand;
+    private Deck playerDeck;
+
+    public Player(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.hp = 80;
+        this.mana = 1;
+        this.hand = new ArrayList<>();
+        this.playerDeck = new Deck();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public int getMana() {
+        return this.mana;
+    }
+
+    public List<Card> getHand() {
+        return this.hand;
+    }
+
+    public Deck getPlayerDeck() {
+        return this.playerDeck;
+    }
+
+    public void reduceHp(int value) {
+        this.hp -= value;
+    }
+
+    public void resetMana() {
+        this.mana = Math.min(GameState.getCurrentRound(), 10);
+    }
+
+    public void reduceMana(int value) {
+        this.mana -= value;
+    }
+}
