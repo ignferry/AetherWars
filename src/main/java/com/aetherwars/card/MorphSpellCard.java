9b8ff96e72cd5d1a1c629f889726e9d078a4d012
Gleen <rahmatrafid22@gmail.com>
2022-04-28 13:14:00 +0700

    debug MorphSpell
    error karena attribut newForm belum dihapus

--- src/main/java/com/aetherwars/card/MorphSpellCard.java
+++ src/main/java/com/aetherwars/card/MorphSpellCard.java
@@ -8,24 +8,10 @@ import java.net.URISyntaxException;
 
 public class MorphSpellCard extends SpellCard {
     private int targetId;
-    private CharacterCard newForm;
     // dilengkapi ya, ini dibuat biar bisa bikin cardList
     public MorphSpellCard(int id, String name, String desc, String imagePath, int mana, int targetId) {
         super(id, name, desc, imagePath, mana, -1, "morph");
         this.targetId = targetId;
-
-//        CardList cl = new CardList();
-//        File charCSV = new File("..\\..\\..\\resources\\com.aetherwars\\card\\data\\character.csv");
-//        try{
-//            cl.loadCharacterCards(charCSV);
-//        }
-//        catch (IOException e) {
-//            ;
-//        }
-//        catch (URISyntaxException e) {
-//            ;
-//        }
-//        this.newForm = cl.getCharCards(targetId);
     }
 
     public int getTargetId(){return this.targetId;}
@@ -38,6 +24,6 @@ public class MorphSpellCard extends SpellCard {
 
     @Override
     public void useSpell(CharacterCard target) {
-        target.changeAll(this.newForm);
+        target.changeAll((CharacterCard) CardList.getById(this.targetId));
     }
 }
